2822bb8386362b3e59783418f865d8364ee0e648
Maxence <estlm084@cegep-rimouski.qc.ca>
2023-02-14 14:37:10 -0500

    test

--- 12-javascript-DOM/scripts/script.js
+++ 12-javascript-DOM/scripts/script.js
@@ -5,16 +5,16 @@
 window.onload = () => {
   // 2. Remplacez le texte du titre par "DOMaine des Arbres"
   
-  document.title = "DOMaine des Arbres"
+  document.title = "DOMaine des Arbres";
   
   // 3. Changez l'ordre de présentation des images avec flex
 
-  document.getElementsByClassName("container")[0].style.flexDirection = "column-reverse"
+  document.getElementsByClassName("container")[0].style.flexDirection = "column-reverse";
 
   // 4. Ajoutez le point "Terre" à la liste d'éléments
 
-  document.getElementsByTagName("ul")[0].append(document.createElement("li"))
-  document.getElementsByTagName("li")[3].append("Terre")
+  document.getElementsByTagName("ul")[0].append(document.createElement("li"));
+  document.getElementsByTagName("li")[3].append("Terre");
 
   // 5. Ajoutez l'attribut href manquant à la balise lien de l'image d'arbre.
   // avec la valeur  href="https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg"
@@ -24,10 +24,9 @@ window.onload = () => {
   )[0];
   // code ici
 
-
+  document.getElementsByClassName("container")[0].lastChild.href = "https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg";
 
   // 6. Modifiez la police du texte pour toutes les éléments de la liste
 
-
-
+    
 };
